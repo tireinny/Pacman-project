b775262b94c4a0b040ecb79e6b9f2eda1e0ca1ec
Long Calamia <lucky93933@csu.fullerton.edu>
2022-10-12 13:02:37 -0700

    fix the button so that it will exit once clicked on

--- start_screen.py
+++ start_screen.py
@@ -27,29 +27,43 @@ class Start_screen:
         self.screen_width = 1000
         self.screen_height =700 
         self.backgroundColor = (0, 0, 0) #Black
+        self.run = False
 
         self.screen = pg.display.set_mode((self.screen_width, self.screen_height)) 
         pg.display.set_caption(f'Pacman Menu')
 
-    def create_button(self,message, x, y, width, height, hover_color, normal_color): # function to create button, x, and y stands for the pixel coordinate, width and height is the dimenstion of the button, hover color is what color
+    def create_button(self,message, x, y, width, height, hover_color, normal_color, option): # function to create button, x, and y stands for the pixel coordinate, width and height is the dimenstion of the button, hover color is what color
                                                                                      # you want your button to be when it is hovered upon, normal_color is what the button color normally is. 
         mouse = pg.mouse.get_pos()# get the mouse's current position on the screen
         press = pg.mouse.get_pressed(3) # detect if mouse is clicked
 
-        if x +width > mouse[0] > x and y + height > mouse[1] >y:  # detect if mouse is hovering over the location of the box
-            pg.draw.rect(self.screen, hover_color, (x,y, width, height)) #turn the box into  the hover color
-            if press[0] ==1:
-                self.finish_start = True
-                #self.title_music.stop()
-        else:
-            pg.draw.rect(self.screen, normal_color, (x, y, width, height))
+        if(option == 1):
+            if x +width > mouse[0] > x and y + height > mouse[1] >y:  # detect if mouse is hovering over the location of the box
+                pg.draw.rect(self.screen, hover_color, (x,y, width, height)) #turn the box into  the hover color
+                if press[0] ==1:
+                    
+                    self.run =True #remove this once Game screen is implemented 
+                    #game.play()
+                    
+            else:
+                pg.draw.rect(self.screen, normal_color, (x, y, width, height))
+        elif(option ==2):
+            if x +width > mouse[0] > x and y + height > mouse[1] >y:  # detect if mouse is hovering over the location of the box
+                pg.draw.rect(self.screen, hover_color, (x,y, width, height)) #turn the box into  the hover color
+                if press[0] ==1:
+                    
+                    self.run =True #remove this once highscore screen is implemented 
+                    #highscore.play()
+                    #self.title_music.stop()
+            else:
+                pg.draw.rect(self.screen, normal_color, (x, y, width, height))
 
         play_button_text = self.small_font.render(message, True, GRAY)
         self.screen.blit(play_button_text, (int(x), int(y)))
 
     def play(self):
-        run = True
-        while (run):
+        
+        while not self.run:
 
             start_text = self.title_font.render("Pac man", True, (255,255,255)) #get fonts ready
             play_text = self.small_font.render("Play Game", True, GRAY)
@@ -62,8 +76,8 @@ class Start_screen:
             
 
             self.screen.blit(start_text, [(self.screen_width - start_text.get_width())/2, 0])
-            self.create_button("PLAY", (self.screen_width -play_text.get_width())/2, self.screen_height-200, 200, 60, LIGHTERGRAY, BLACK)
-            self.create_button("HighScores", (self.screen_width -highscore_text.get_width())/2, self.screen_height-100, 510, 60, LIGHTERGRAY, BLACK)
+            self.create_button("PLAY", (self.screen_width -play_text.get_width())/2, self.screen_height-200, 200, 60, LIGHTERGRAY, BLACK, 1)
+            self.create_button("HighScores", (self.screen_width -highscore_text.get_width())/2, self.screen_height-100, 510, 60, LIGHTERGRAY, BLACK, 2)
 
 
             names_of_ghosts = '-SHADOW\n "BLINKY"\n -SPEEDY\n "PINKY"\n -BASHFUL \n"INKYl"\n -POKEY \n"CLYDE"'.splitlines()
@@ -83,17 +97,20 @@ class Start_screen:
             pos_indexY =0
 
             while(self.blink_ghost_names == False): #responsible for blinking the ghosts and names sequentially 
-               
+                
                 for n in list_of_ghost: # loop used for drawing ghosts on screen before their name is introduced
                     for g in range(2):
                         if g == 0:
+                            
                             n.draw(list_of_posX[pos_indexX], list_of_posY[pos_indexY])
                             n.update() 
+                           
+
                             # pg.display.update()
                 # if color_index%2 ==0:
                 #     ghost.red_ghost.draw(list_of_posX[pos_indexX], list_of_posY[pos_indexY])
                 #     ghost.red_ghost.update() 
-                        # pg.display.update()
+                        pg.display.update()
                         pg.time.wait(700) #timer for duration between each blink
                         if len(names_of_ghosts) <= txt_cnt: #break out of loop if the there are no more ghost names
                             self.blink_ghost_names = True
@@ -105,14 +122,14 @@ class Start_screen:
                         self.screen.blit(text, (list_of_posX[pos_indexX], list_of_posY[pos_indexY]))
                         # [self.screen.blit(redghost.sprites[n]) for n in len(redghost.sprites)]
                         
-                        
-                        pg.display.update()
+                      
                         
                         pos_indexY +=1
                         pos_indexX +=1
                         if pos_indexX > 1: pos_indexX=0 #reset pos x after the nickname is shown
                         if pos_indexY > 7: pos_indexY =0#reset pos y after the nickname is shown
                         txt_cnt +=1
+               
             
             # for n in list_of_ghost:
             #     for i in range(2):
