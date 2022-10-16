78ccbe14d6a040fa05e49058b3806e7abd370ceb
Khoi <115506492+RicyRice@users.noreply.github.com>
2022-10-15 20:31:55 -0700

    Update ghosts.py

--- ghosts.py
+++ ghosts.py
@@ -2,100 +2,247 @@ import pygame
 # Demo Only 
 # From a different resource file
 # -------------------------------------------------------------------------------------
-class Enemy:
-    SPEED = 6
+class Ghost:
+    def __init__(self, row, col, color, changeFeetCount):
+        self.row = row
+        self.col = col
+        self.attacked = False
+        self.color = color
+        self.dir = randrange(4)
+        self.dead = False
+        self.changeFeetCount = changeFeetCount
+        self.changeFeetDelay = 5
+        self.target = [-1, -1]
+        self.ghostSpeed = 1/4
+        self.lastLoc = [-1, -1]
+        self.attackedTimer = 240
+        self.attackedCount = 0
+        self.deathTimer = 120
+        self.deathCount = 0
 
-    def __init__(self, rect, velocity=Vector()):
-        self.enemyAnimation = [# png files go here for animations.]
-        self.currentFrame, self.animationDirection, self.portalPower = 0, 0, 0
-        self.startF, self.endF = 0, 1  # len(self.enemyAnimation) - 1
-        self.rect = rect
-        self.velocity = velocity
-        self.enemy = pg.Rect(300, 100, 50, 50)
-        self.image = pg.transform.rotozoom(pg.image.load(self.enemyAnimation[self.currentFrame]), 0, 0.06)
+    def update(self):
+        # print(self.row, self.col)
+        if self.target == [-1, -1] or (self.row == self.target[0] and self.col == self.target[1]) or gameBoard[int(self.row)][int(self.col)] == 4 or self.dead:
+            self.setTarget()
+        self.setDir()
+        self.move()
 
-    def __repr__(self):
-        return "Enemy(rect={},velocity={})".format(self.rect, self.velocity)
+        if self.attacked:
+            self.attackedCount += 1
 
-    def change_frame(self):
-        if self.velocity == Vector():
-            return
-        if self.startF <= self.currentFrame < self.endF:
-            self.currentFrame += 1
-        else:
-            self.currentFrame = self.startF
+        if self.attacked and not self.dead:
+            self.ghostSpeed = 1/8
 
-    def change_menu_frame(self):
-        if self.startF <= self.currentFrame < self.endF:
-            self.currentFrame += 1
-        else:
-            self.currentFrame = self.startF
+        if self.attackedCount == self.attackedTimer and self.attacked:
+            if not self.dead:
+                self.ghostSpeed = 1/4
+                self.row = math.floor(self.row)
+                self.col = math.floor(self.col)
 
-    def limit_to_screen(self, game):
-        self.rect.top = max(73, min(game.WINDOW_HEIGHT - self.rect.height - 55, self.rect.top))
-        if game.m == 0:
-            self.rect.left = max(-28, min(game.WINDOW_WIDTH - self.rect.width + 48, self.rect.left))
-        else:
-            self.rect.left = max(-300, min(game.WINDOW_WIDTH - self.rect.width + 48, self.rect.left))
+            self.attackedCount = 0
+            self.attacked = False
+            self.setTarget()
 
-    def move(self, game):
-        if self.velocity == Vector():
-            return
-        if game.level >= 3:
-            self.portalPower = 1
-        if game.m == 1:
-            self.rect.left += self.velocity.x
-            self.rect.top += self.velocity.y
-            tempX = self.velocity.x
-            tempY = self.velocity.y
-            if tempX != 0 or tempY != 0:
-                self.change_frame()
-        else:
-            self.change_frame()
-            if not self.check_collisions(game):
-                self.rect.left += self.velocity.x
-                self.rect.top += self.velocity.y
-            if self.check_collisions(game):
-                self.rect.left -= self.velocity.x
-                self.rect.top -= self.velocity.y
-            if game.bluemode == 0:
-                if self.velocity.x > 0:
-                    self.startF, self.endF = 2, 3
-                elif self.velocity.x < 0:
-                    self.startF, self.endF = 0, 1
-                elif self.velocity.y > 0:
-                    self.startF, self.endF = 4, 5
+        if self.dead and gameBoard[self.row][self.col] == 4:
+            self.deathCount += 1
+            self.attacked = False
+            if self.deathCount == self.deathTimer:
+                self.deathCount = 0
+                self.dead = False
+                self.ghostSpeed = 1/4
+
+    def draw(self): # Ghosts states: Alive, Attacked, Dead Attributes: Color, Direction, Location
+        ghostImage = pygame.image.load(ElementPath + "tile152.png")
+        currentDir = ((self.dir + 3) % 4) * 2
+        if self.changeFeetCount == self.changeFeetDelay:
+            self.changeFeetCount = 0
+            currentDir += 1
+        self.changeFeetCount += 1
+        if self.dead:
+            tileNum = 152 + currentDir
+            ghostImage = pygame.image.load(ElementPath + "tile" + str(tileNum) + ".png")
+        elif self.attacked:
+            if self.attackedTimer - self.attackedCount < self.attackedTimer//3:
+                if (self.attackedTimer - self.attackedCount) % 31 < 26:
+                    ghostImage = pygame.image.load(ElementPath + "tile0" + str(70 + (currentDir - (((self.dir + 3) % 4) * 2))) + ".png")
                 else:
-                    self.startF, self.endF = 6, 7
+                    ghostImage = pygame.image.load(ElementPath + "tile0" + str(72 + (currentDir - (((self.dir + 3) % 4) * 2))) + ".png")
             else:
-                self.startF, self.endF = 8, 9
-        self.limit_to_screen(game)
+                ghostImage = pygame.image.load(ElementPath + "tile0" + str(72 + (currentDir - (((self.dir + 3) % 4) * 2))) + ".png")
+        else:
+            if self.color == "blue":
+                tileNum = 136 + currentDir
+                ghostImage = pygame.image.load(ElementPath + "tile" + str(tileNum) + ".png")
+            elif self.color == "pink":
+                tileNum = 128 + currentDir
+                ghostImage = pygame.image.load(ElementPath + "tile" + str(tileNum) + ".png")
+            elif self.color == "orange":
+                tileNum = 144 + currentDir
+                ghostImage = pygame.image.load(ElementPath + "tile" + str(tileNum) + ".png")
+            elif self.color == "red":
+                tileNum = 96 + currentDir
+                if tileNum < 100:
+                    ghostImage = pygame.image.load(ElementPath + "tile0" + str(tileNum) + ".png")
+                else:
+                    ghostImage = pygame.image.load(ElementPath + "tile" + str(tileNum) + ".png")
 
-    def check_collisions(self, game):
-        if game.bluePortal.active == 1 and game.oranPortal.active == 1 and self.portalPower == 1:
-            if self.rect.colliderect(game.bluePortal.rect):
-                game.audio.play_sound(4)
-                self.rect.left, self.rect.top = game.oranPortal.rect.left, game.oranPortal.rect.top
-                game.bluePortal.remove_portal()
-                game.oranPortal.remove_portal(400)
-            if self.rect.colliderect(game.oranPortal.rect):
-                game.audio.play_sound(4)
-                self.rect.left, self.rect.top = game.bluePortal.rect.left, game.bluePortal.rect.top
-                game.bluePortal.remove_portal()
-                game.oranPortal.remove_portal(400)
+        ghostImage = pygame.transform.scale(ghostImage, (int(square * spriteRatio), int(square * spriteRatio)))
+        screen.blit(ghostImage, (self.col * square + spriteOffset, self.row * square + spriteOffset, square, square))
 
-        for j in range(len(game.gWalls)):
-            if self.rect.colliderect(game.gWalls[j]):
+    def isValidTwo(self, cRow, cCol, dist, visited):
+        if cRow < 3 or cRow >= len(gameBoard) - 5 or cCol < 0 or cCol >= len(gameBoard[0]) or gameBoard[cRow][cCol] == 3:
+            return False
+        elif visited[cRow][cCol] <= dist:
+            return False
+        return True
+
+    def isValid(self, cRow, cCol):
+        if cCol < 0 or cCol > len(gameBoard[0]) - 1:
+            return True
+        for ghost in game.ghosts:
+            if ghost.color == self.color:
+                continue
+            if ghost.row == cRow and ghost.col == cCol and not self.dead:
+                return False
+        if not ghostGate.count([cRow, cCol]) == 0:
+            if self.dead and self.row < cRow:
                 return True
-        return False
+            elif self.row > cRow and not self.dead and not self.attacked and not game.lockedIn:
+                return True
+            else:
+                return False
+        if gameBoard[cRow][cCol] == 3:
+            return False
+        return True
 
-    def draw(self, game):
-        self.image = pg.transform.rotozoom(pg.image.load(self.enemyAnimation[self.currentFrame]), 0, 0.06)
-        game.surface.blit(self.image, self.rect)
+    def setDir(self): #Very inefficient || can easily refactor
+        # BFS search -> Not best route but a route none the less
+        dirs = [[0, -self.ghostSpeed, 0],
+                [1, 0, self.ghostSpeed],
+                [2, self.ghostSpeed, 0],
+                [3, 0, -self.ghostSpeed]
+        ]
+        random.shuffle(dirs)
+        best = 10000
+        bestDir = -1
+        for newDir in dirs:
+            if self.calcDistance(self.target, [self.row + newDir[1], self.col + newDir[2]]) < best:
+                if not (self.lastLoc[0] == self.row + newDir[1] and self.lastLoc[1] == self.col + newDir[2]):
+                    if newDir[0] == 0 and self.col % 1.0 == 0:
+                        if self.isValid(math.floor(self.row + newDir[1]), int(self.col + newDir[2])):
+                            bestDir = newDir[0]
+                            best = self.calcDistance(self.target, [self.row + newDir[1], self.col + newDir[2]])
+                    elif newDir[0] == 1 and self.row % 1.0 == 0:
+                        if self.isValid(int(self.row + newDir[1]), math.ceil(self.col + newDir[2])):
+                            bestDir = newDir[0]
+                            best = self.calcDistance(self.target, [self.row + newDir[1], self.col + newDir[2]])
+                    elif newDir[0] == 2 and self.col % 1.0 == 0:
+                        if self.isValid(math.ceil(self.row + newDir[1]), int(self.col + newDir[2])):
+                            bestDir = newDir[0]
+                            best = self.calcDistance(self.target, [self.row + newDir[1], self.col + newDir[2]])
+                    elif newDir[0] == 3 and self.row % 1.0 == 0:
+                        if self.isValid(int(self.row + newDir[1]), math.floor(self.col + newDir[2])):
+                            bestDir = newDir[0]
+                            best = self.calcDistance(self.target, [self.row + newDir[1], self.col + newDir[2]])
+        self.dir = bestDir
 
-    def update(self, game):
-        self.check_collisions(game=game)
-        self.move(game=game)
-        self.draw(game=game)
+    def calcDistance(self, a, b):
+        dR = a[0] - b[0]
+        dC = a[1] - b[1]
+        return math.sqrt((dR * dR) + (dC * dC))
+
+    def setTarget(self):
+        if gameBoard[int(self.row)][int(self.col)] == 4 and not self.dead:
+            self.target = [ghostGate[0][0] - 1, ghostGate[0][1]+1]
+            return
+        elif gameBoard[int(self.row)][int(self.col)] == 4 and self.dead:
+            self.target = [self.row, self.col]
+        elif self.dead:
+            self.target = [14, 13]
+            return
+
+        # Records the quadrants of each ghost's target
+        quads = [0, 0, 0, 0]
+        for ghost in game.ghosts:
+            # if ghost.target[0] == self.row and ghost.col == self.col:
+            #     continue
+            if ghost.target[0] <= 15 and ghost.target[1] >= 13:
+                quads[0] += 1
+            elif ghost.target[0] <= 15 and ghost.target[1] < 13:
+                quads[1] += 1
+            elif ghost.target[0] > 15 and ghost.target[1] < 13:
+                quads[2] += 1
+            elif ghost.target[0]> 15 and ghost.target[1] >= 13:
+                quads[3] += 1
+
+        # Finds a target that will keep the ghosts dispersed
+        while True:
+            self.target = [randrange(31), randrange(28)]
+            quad = 0
+            if self.target[0] <= 15 and self.target[1] >= 13:
+                quad = 0
+            elif self.target[0] <= 15 and self.target[1] < 13:
+                quad = 1
+            elif self.target[0] > 15 and self.target[1] < 13:
+                quad = 2
+            elif self.target[0] > 15 and self.target[1] >= 13:
+                quad = 3
+            if not gameBoard[self.target[0]][self.target[1]] == 3 and not gameBoard[self.target[0]][self.target[1]] == 4:
+                break
+            elif quads[quad] == 0:
+                break
+
+    def move(self):
+        # print(self.target)
+        self.lastLoc = [self.row, self.col]
+        if self.dir == 0:
+            self.row -= self.ghostSpeed
+        elif self.dir == 1:
+            self.col += self.ghostSpeed
+        elif self.dir == 2:
+            self.row += self.ghostSpeed
+        elif self.dir == 3:
+            self.col -= self.ghostSpeed
+
+        # Incase they go through the middle tunnel
+        self.col = self.col % len(gameBoard[0])
+        if self.col < 0:
+            self.col = len(gameBoard[0]) - 0.5
+
+
+
+    def setAttacked(self, isAttacked):
+        self.attacked = isAttacked
+
+    def isAttacked(self):
+        return self.attacked
+
+    def setDead(self, isDead):
+        self.dead = isDead
+        
+    def isDead(self):
+        return self.dead
+
+game = Game(1, 0)
+ghostsafeArea = [15, 13] # The location the ghosts escape to when attacked
+ghostGate = [[15, 13], [15, 14]]
+
+def canMove(row, col):
+    if col == -1 or col == len(gameBoard[0]):
+        return True
+    if gameBoard[int(row)][int(col)] != 3:
+        return True
+    return False
+
+# Reset after death
+def reset():
+    global game
+    game.ghosts = [Ghost(14.0, 13.5, "red", 0), Ghost(17.0, 11.5, "blue", 1), Ghost(17.0, 13.5, "pink", 2), Ghost(17.0, 15.5, "orange", 3)]
+    for ghost in game.ghosts:
+        ghost.setTarget()
+    game.pacman = Pacman(26.0, 13.5)
+    game.lives -= 1
+    game.paused = True
+    game.render()
 
 # -------------------------------------------------------------------------------------
