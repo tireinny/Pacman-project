01fbe1e60621ec1ba3f26a13f54184d3fa773479
Khoi <115506492+RicyRice@users.noreply.github.com>
2022-10-16 18:08:40 -0700

    Update settings.py
    Update to match pacman.

--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
+# Please check for me. Thank you - Khoi 
+
 import random
 class Settings():
     """A class to store all settings for Alien Invasion."""
@@ -5,37 +7,23 @@ class Settings():
     def __init__(self):
         """Initialize the game's settings."""
         # Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (150, 150, 150)
+        self.screen_width = 224
+        self.screen_height = 288
+
+        # changes I made 
+        # shoot portal every 4 minutes 
+        # set points for ghosts 
         
-# # TODO: test laser with a really wide laser
-        self.laser_width = 5
-        self.laser_height = 30
-        self.laser_color = 255, 0, 0
-        self.lasers_every = 10           # change to 1 to see faster lasers
-
-        self.aliens_shoot_every = 120    # about every 2 seconds at 60 fps
-        self.alien_points = 50
-        self.alien1_points = 100
-        self.alien2_points = 150
-        self.ufo_points = random.randint(100,200)
-
-
-
-# # TODO: set a ship_limit of 3
-        self.ship_limit = 3         # total ships allowed in game before game over
-
-        self.fleet_drop_speed = 1
-        self.fleet_direction = 1     # change to a Vector(1, 0) move to the right, and ...
-        self.initialize_speed_settings()
-
+        self.pacman_shoot_every = 240    # about every 240 seconds (4 minutes) at 60 fps 
+        self.ghosts = 50
+        self.fruits = random.randint(screen_width, screen_height)           # spawn random locations
+        
     def initialize_speed_settings(self):
-        self.alien_speed_factor = 1
-        self.ship_speed_factor = 1
-        self.laser_speed_factor = 1
+        self.ghosts_speed = 1
+        self.pacman_speed_factor = 1
+        self.portal_speed_factor = 1
 
     def increase_speed(self):
         scale = self.speedup_scale
-        self.ship_speed_factor *= scale
-        self.laser_speed_factor *= scale
+        self.pacman_speed_factor *= scale
+        self.portal_speed_factor *= scale
