26e814e427d6efa804eae630416b0263f7f6f94e
tireinny <charlestuong150@gmail.com>
2022-10-16 23:46:26 -0700

    minor modifications to settings

--- settings.py
+++ settings.py
@@ -1,4 +1,7 @@
 # Please check for me. Thank you - Khoi 
+'''Modified ghost point value to be 200 (which doubles if you eat ghosts quickly, which means we need a timer.py). 
+Also added node point value, as each dot is worth 10 in the original, 
+and modified screen width and height variables to be same as game.py -Charlie'''
 
 import random
 class Settings():
@@ -6,17 +9,16 @@ class Settings():
 
     def __init__(self):
         """Initialize the game's settings."""
-        # Screen settings
-        self.screen_width = 400
-        self.screen_height = 600
-
+        self.screen_width = 800
+        self.screen_height = 1000
         # changes I made 
         # shoot portal every 4 minutes 
         # set points for ghosts 
         
         self.pacman_shoot_every = 240    # about every 240 seconds (4 minutes) at 60 fps 
-        self.ghosts = 50
-        self.fruits = random.randint(screen_width, screen_height)           # spawn random locations
+        self.ghost_points = 200 
+        self.node_points = 10 
+        self.fruits = random.randint(self.screen_width, self.screen_height)           # spawn random locations
         
     def initialize_speed_settings(self):
         self.ghosts_speed = 1
