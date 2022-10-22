ed08292a5ae06ea1083c9287bb7a723a93a08695
tireinny <charlestuong150@gmail.com>
2022-10-22 15:10:41 -0700

    update pacman, character, and spritesheets

--- character.py
+++ character.py
@@ -1,6 +1,19 @@
-class Character:
-    def __init__(self):pass
-    def spawn(self):pass
+import pygame as pg
+from pygame.sprite import Sprite
+
+class Character(Sprite):
+    pacman_down =  [pg.transform.rotozoom(pg.image.load(f'images/sprites/pacman/pacman_down{n}.png'), 0, 1.0) for n in range(2)]
+    pacman_up =  [pg.transform.rotozoom(pg.image.load(f'images/sprites/pacman/pacman_up{n}.png'), 0, 1.0) for n in range(2)]
+    pacman_left =  [pg.transform.rotozoom(pg.image.load(f'images/sprites/pacman/pacman_left{n}.png'), 0, 1.0) for n in range(2)]
+    pacman_right =  [pg.transform.rotozoom(pg.image.load(f'images/sprites/pacman/pacman_right{n}.png'), 0, 1.0) for n in range(2)]
+    pacman_dead =  [pg.transform.rotozoom(pg.image.load(f'images/sprites/dead/dead{n}.png'), 0, 1.0) for n in range(16)]
+
+    def __init__(self, game):
+        super.__init__()
+        self.game = game
+
     def die(self):pass
     def update(self):pass
-    def draw(self):pass
+    def draw(self, pos_x, pos_y):
+        self.rect = self.image.get_rect()
+        
--- pacman.py
+++ pacman.py
@@ -1,26 +1,23 @@
 import pygame as pg
-import pygame.sprite as Sprite
+from character import Character
+from vector import Vector
+import timer
 
 
-class Pacman(Sprite):
-    def __init__(self, game, screen, row, col):
+class Pacman(Character):
+    def __init__(self):
         super.__init__()
-        self.screen = screen
-        self.game = game
-        self.row = row
-        self.col = col
-        self.rect = self.image.get_rect()
 
     def center_pac(self):
         self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+        return Vector(self.rect.left, self.rect.top)
 
+    def fire_portal():
+        pass
 
-    def draw(self):
+    def reset(self):
         pass
 
     def update(self):
         pass
-
-
-
-
